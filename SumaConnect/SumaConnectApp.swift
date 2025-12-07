bc13b90f60e05123ef8d2ba4a431e2fdc95ce078
Alberto Orrantia <ricco.albert@gmail.com>
2025-12-07 15:24:27 -0800

    Initial XCode project setup

---
+++ SumaConnect/SumaConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumaConnectApp.swift
+//  SumaConnect
+//
+//  Created by Alberto Orrantia on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SumaConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
