bc78d2a7bdf7a9dc13e5781933aa0b36e5f08f23
Alberto Orrantia <ricco.albert@gmail.com>
2025-12-07 21:23:49 -0800

    Add Suma color palette and color extension

---
+++ SumaConnect/DesignSystem/Colors/Color+Suma.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Suma.swift
+//  SumaConnect
+//
+//  Created by Alberto Orrantia on 07/12/25.
+//
+
+import SwiftUI
+
+extension Color {
+    enum Suma {
+        static let red  = Color("SumaRed")
+        static let darkBlue = Color("SumaDarkBlue")
+        static let yellow = Color("SumaYellow")
+        static let white = Color("SumaWhite")
+        static let pinkRed = Color("SumaPinkRed")
+        static let lightBlue = Color("SumaLightBlue")
+    }
+    
+}
